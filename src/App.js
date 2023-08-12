ea54ec505edb557a2fd11f3b10a46626a0286ad7
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-13 03:24:48 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
 function App() {
+  const [username,setUsername]=useState('');
+  const [userData,setUserData]=useState(null);
+    const fetchProfile=async ()=>{
+      try {
+        const url=await fetch(`https://api.github.com/users/${username}`);
+      const data=await url.json();
+      setUserData(data);
+      } catch (error) {
+        console.log('error');
+      }
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt="" />
+        <h1><span>Git</span>Finder</h1>
+      </div>
+      <div className="middle">
+        <div className="search-bar">
+          <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='search name'/>
+          <button onClick={fetchProfile}>Search</button>
+        </div>
+      </div>
+      <div className="main-profile">
+        <div className="box">
+          {userData && (
+        <div className="user-profile">
+          <img src={userData.avatar_url} alt="Profile Avatar" />
+          <h2>{userData.login}</h2>
+          <p>{userData.bio}</p>
+          <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+            Visit Profile
+          </a>
+        </div>
+      )}
+        </div>
+      </div>
     </div>
   );
 }
