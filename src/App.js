7f96fca2a24e0e199c4cad6bb093274af3a8ad5f
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-14 03:57:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 function App() {
-  const [username,setUsername]=useState('');
-  const [userData,setUserData]=useState(null);
-    const fetchProfile=async ()=>{
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState(null);
+  const [defaultProfiles, setDefaultProfiles] = useState([]);
+  useEffect(() => {
+    const defaultProfile = async () => {
       try {
-        const url=await fetch(`https://api.github.com/users/${username}`);
-      const data=await url.json();
-      setUserData(data);
+        const response = await axios.get(`https://api.github.com/users`);
+        setDefaultProfiles(response.data);
       } catch (error) {
-        console.log('error');
+        console.log('error', error);
       }
     }
+    defaultProfile();
+  });
+  const fetchProfile = async () => {
+    try {
+      const url = await axios.get(`https://api.github.com/users/${username}`);
+      const data = await url.data;
+      setUserData(data);
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
   return (
     <div className="App">
       <div className="navbar">
@@ -20,23 +33,41 @@ function App() {
       </div>
       <div className="middle">
         <div className="search-bar">
-          <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='search name'/>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='search name' />
           <button onClick={fetchProfile}>Search</button>
         </div>
       </div>
       <div className="main-profile">
         <div className="box">
           {userData && (
-        <div className="user-profile">
-          <img src={userData.avatar_url} alt="Profile Avatar" />
-          <h2>{userData.login}</h2>
-          <p>{userData.bio}</p>
-          <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
-            Visit Profile
-          </a>
+            <div className="user-profile">
+              <img src={userData.avatar_url} alt="Profile Avatar" />
+              <h2>{userData.login}</h2>
+              <p>{userData.bio}</p>
+              <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+                Visit Profile
+              </a>
+            </div>
+          )}
         </div>
-      )}
-        </div>
+        {
+           userData==null && defaultProfiles.length>0&&(
+            <div className="default-profile-main">
+              <h2>Top Profiles</h2>
+              <div className="default-profiles">
+                {defaultProfiles.map(profile => (
+                  <div key={profile.id} className="default-profile">
+                    <img src={profile.avatar_url} alt="Profile Avatar" />
+                    <h3>{profile.login}</h3>
+                    <a href={profile.html_url} target="_blank" rel="noopener noreferrer">
+                      Visit Profile
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )
+        }
       </div>
     </div>
   );
