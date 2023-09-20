a8c253faaaef5b07f56f341ae0b761dde3447f0b
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-09-21 03:09:04 +0530

    added style and some functionality

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Navbar from './Components/navbar';
+import Repo from './Components/repo';
+import Main from './Components/Main';
 function App() {
-  const [username, setUsername] = useState('');
-  const [userData, setUserData] = useState(null);
-  const [defaultProfiles, setDefaultProfiles] = useState([]);
-  useEffect(() => {
-    const defaultProfile = async () => {
-      try {
-        const response = await axios.get(`https://api.github.com/users`);
-        setDefaultProfiles(response.data);
-      } catch (error) {
-        console.log('error', error);
-      }
-    }
-    defaultProfile();
-  });
-  const fetchProfile = async () => {
-    try {
-      const url = await axios.get(`https://api.github.com/users/${username}`);
-      const data = await url.data;
-      setUserData(data);
-    } catch (error) {
-      console.log('error', error);
-    }
-  }
+  // const [username, setUsername] = useState('');
+  // const [userData, setUserData] = useState(null);
+  // const [defaultProfiles, setDefaultProfiles] = useState([]);
+  // useEffect(() => {
+  //   const defaultProfile = async () => {
+  //     try {
+  //       const response = await axios.get(`https://api.github.com/users`);
+  //       setDefaultProfiles(response.data);
+  //     } catch (error) {
+  //       console.log('error', error);
+  //     }
+  //   }
+  //   defaultProfile();
+  // });
+  // const fetchProfile = async () => {
+  //   try {
+  //     const url = await axios.get(`https://api.github.com/users/${username}`);
+  //     const data = await url.data;
+  //     setUserData(data);
+  //   } catch (error) {
+  //     console.log('error', error);
+  //   }
+  // }
   return (
     <div className="App">
-      <div className="navbar">
+      {/* <div className="navbar">
+        <div className="logo">
         <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt="" />
+        <div className="header">
         <h1><span>Git</span>Finder</h1>
-      </div>
-      <div className="middle">
+        </div>
+        </div>
+        <div className="list-item">
+          <ul>
+            <li><a>Home</a></li>
+            <li><a href="">Repository</a></li>
+            <li><a href="">Help</a></li>
+          </ul>
+        </div>
+      </div> */}
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/Main' element={<Main/>} />
+          <Route path='/repo' element={<Repo/>}/>
+        </Routes>
+      </Router>
+      {/* <div className="middle">
         <div className="search-bar">
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='search name' />
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Search Username' />
           <button onClick={fetchProfile}>Search</button>
         </div>
-      </div>
-      <div className="main-profile">
+      </div> */}
+      {/* <div className="main-profile">
         <div className="box">
           {userData && (
             <div className="user-profile">
@@ -68,7 +90,7 @@ function App() {
             </div>
           )
         }
-      </div>
+      </div> */}
     </div>
   );
 }
